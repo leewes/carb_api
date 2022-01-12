56bdecc69e8d59dfb83ce4d8298c1054e4425585
Wesley L <wesley63265@gmail.com>
2022-01-12 15:54:09 +0900

    Add POST, PUT, DELETE endpoints for carbs.

--- app/controllers/api/carbs_controller.rb
+++ app/controllers/api/carbs_controller.rb
@@ -1,6 +1,53 @@
 class Api::CarbsController < ApplicationController
+    before_action :find_carb, only: [:show, :update, :destroy]
+
     def index
         @carb = Carb.all
         render json: @carb
+    end
+
+    # GET /carbs/:id
+    def show
+        render json: @carb
+    end
+
+    # POST /carbs
+    def create
+        @carb = Carb.new(carb_params)
+        if @Carb.save
+            render json: @carb
+        else
+            render error: { error: 'Error in POST. Cannot create new carb.'}, status: 400
+        end
+    end
+
+    # PUT /users/:id
+    def update
+        if @carb
+            @carb.update(carb_params)
+            render json: { message: 'Carb was updated.'}, status: 200
+        else
+            render json: { error: 'Error in PUT. Cannot update carb.'}, status: 400
+        end
+    end
+
+    # DELETE /carbss/:id
+    def destroy
+        if @carb
+            @carb.destroy
+            render json: { message: 'Carb was deleted.'}, status: 200
+        else
+            render error: { error: 'Error in DELETE. Cannot delete carb.'}, status: 400
+        end
+    end
+
+    private
+
+    def find_carb
+        @carb = Carb.find(params[:id])
+    end
+
+    def carb_params
+        params.require(:carb).permit(:breakfast, :lunch, :dinner)
     end
 end
